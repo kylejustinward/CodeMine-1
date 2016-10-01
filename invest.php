bf2705db2d01bfbd6103954dc64355164103e7c7
kylejustinward <guardianangelmedia@gmail.com>
2016-10-01 00:39:39 -0600

    It worked until I got to the part about net

--- invest.php
+++ invest.php
@@ -5,15 +5,14 @@
 	<!-- USING JS -->
 	  
 	<form>
-		Address<input type = "text" name = "address" id="address">
+		Address<input type = "text" name = "address" id="address"/>
 		Square Feet<input type = "number" name = "squareFeet" id="squareFeet">
 		Price<input type = "number" name = "price" id="price"><br>
 		Price Per Square Feet<p  id="pricePerSquareFeet"></p>
 		
 		Closing Cost<p  id="closingCost"></p>
 		Agents Fee<p  id="agentFee"></p>
-		Cost of Repairs<p  id="repairs"></p>
-		<input type = "submit" name = "investBtn"/>	
+		Cost of Repairs<input type = "number" name = "repairCost" id="repairs"/>
 		<hr>
 		Net<p  id="net"></p>
 		Closing Cost<p  id="closingCost"></p>
@@ -29,9 +28,20 @@
 				var price   = document.getElementById("price").value;
 				var sqrFt   = document.getElementById("squareFeet").value;
 				var priceSqrFt = price / sqrFt;
+				var closingCost = 0.03 * price;
+				var agentFee = 0.06 * price;
+				//var repairCost = document.getElementById("repair").value;
+				//var net = price - ( closingCost + agentFee + repairCost);
 				document.getElementById("pricePerSquareFeet").innerHTML =  priceSqrFt;
+				
+				document.getElementById("closingCost").innerHTML = closingCost;
+
+				document.getElementById("agentFee").innerHTML = agentFee;
+
+				//document.getElementById("net").innerHTML = repairCost;
 				return false;
-			}	
+			}
+
 		</script>	
 	</form>
 </html>
